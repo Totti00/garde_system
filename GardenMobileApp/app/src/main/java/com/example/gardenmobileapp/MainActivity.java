43e84b401a3e4ff509062dc596d95c9f6b2b3127
JBmanu <buizomanuel@gmail.com>
2022-06-12 20:08:54 +0200

    end

--- GardenMobileApp/app/src/main/java/com/example/gardenmobileapp/MainActivity.java
+++ GardenMobileApp/app/src/main/java/com/example/gardenmobileapp/MainActivity.java
@@ -1,29 +1,29 @@
 package com.example.gardenmobileapp;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.gardenmobileapp.State.StateGarden;
 import com.example.gardenmobileapp.system.IrrigationSystem;
 import com.example.gardenmobileapp.system.LightSystem;
+import com.example.gardenmobileapp.system.StateSystem;
 import com.example.gardenmobileapp.view.CounterView;
 
 public class MainActivity extends AppCompatActivity {
+    private StateSystem stateSystem;
     private LightSystem lightSystem;
     private IrrigationSystem irrigationSystem;
-
-    private Button allerBtn;
-    private Button manualBtn;
-    private TextView textState;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.stateSystem = new StateSystem();
+        this.stateSystem.setAllerBtn(findViewById(R.id.allerBtn));
+        this.stateSystem.setTextView(findViewById(R.id.textState));
+        this.stateSystem.setManualBtn(findViewById(R.id.requireManualBtn));
+        this.stateSystem.build();
 
         this.lightSystem = new LightSystem();
         final CounterView led3View = new CounterView(findViewById(R.id.incrLed3), findViewById(R.id.textLed3), findViewById(R.id.decrLed3));
@@ -38,16 +38,7 @@ public class MainActivity extends AppCompatActivity {
         this.irrigationSystem.setIrrigationView(findViewById(R.id.irrigationBtn));
         final CounterView counterView = new CounterView(findViewById(R.id.incrIrrig), findViewById(R.id.textIrrig), findViewById(R.id.decrIrrig));
         this.irrigationSystem.setCounterView(counterView);
-
-        this.allerBtn = findViewById(R.id.allerBtn);
-        this.manualBtn = findViewById(R.id.requireManualBtn);
-        this.textState = findViewById(R.id.textState);
-
-        this.build();
+        this.irrigationSystem.build();
     }
 
-    private void build(){
-        this.allerBtn.setOnClickListener(l -> this.textState.setText(StateGarden.AUTOMATIC.getName()));
-        this.manualBtn.setOnClickListener(l -> this.textState.setText(StateGarden.MANUAL.getName()));
-    }
 }
--- GardenMobileApp/app/src/main/java/com/example/gardenmobileapp/system/StateSystem.java
+++ GardenMobileApp/app/src/main/java/com/example/gardenmobileapp/system/StateSystem.java
@@ -1,4 +1,37 @@
 package com.example.gardenmobileapp.system;
 
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.gardenmobileapp.State.StateGarden;
+
 public class StateSystem {
+    private StateGarden state;
+
+    private Button allerBtn;
+    private Button manualBtn;
+    private TextView textView;
+
+    public StateSystem(){
+        this.state = StateGarden.AUTOMATIC;
+    }
+
+    public void setAllerBtn(Button view) { this.allerBtn = view; }
+    public void setManualBtn(Button view) { this.manualBtn = view; }
+    public void setTextView(TextView view) { this.textView = view; }
+
+    public void build() {
+        this.allerBtn.setOnClickListener(l -> {
+            this.state = StateGarden.AUTOMATIC;
+            this.setTextStateInView();
+        });
+        this.manualBtn.setOnClickListener(l -> {
+            this.state = StateGarden.MANUAL;
+            this.setTextStateInView();
+        });
+
+        this.setTextStateInView();
+    }
+
+    public void setTextStateInView() { this.textView.setText(this.state.getName()); }
 }
